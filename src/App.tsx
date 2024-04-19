32e8adb7566f684bb7ea2d5dbd6f8eaf018078f2
chandanch <chandan.cec107@gmail.com>
2024-04-19 20:50:38 +0530

    update prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Personal Todos App
-        </p>
-
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>Personal Todos App</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
