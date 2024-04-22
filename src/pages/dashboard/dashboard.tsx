d481ac44e88b7fb402c09ae068b7c0b06e1bb484
chandanch <chandan.cec107@gmail.com>
2024-04-22 09:28:29 +0530

    create dashboard page and add grid layout :bulb:

---
+++ src/pages/dashboard/dashboard.tsx
@@ -0,0 +1,34 @@
+import { Grid } from '@mui/material';
+import React, { FC, ReactElement } from 'react';
+
+const Dashboard: FC = (): ReactElement => {
+    return (
+        <Grid container minHeight="100vh" m={0} p={0}>
+            <Grid item md={8} sm={12} xs={12}>
+                <h2>Todos List Area</h2>
+            </Grid>
+            <Grid
+                item
+                md={4}
+                sm={12}
+                xs={12}
+                sx={{
+                    height: '100vh',
+                    position: 'fixed',
+                    right: 0,
+                    top: 0,
+                    width: '100%',
+                    backgroundColor: 'background.paper',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <h2>Todos Create & profile </h2>
+            </Grid>
+        </Grid>
+    );
+};
+
+export default Dashboard;
