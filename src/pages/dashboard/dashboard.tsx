7d00db330445552578c151cfc99e3cddbe754b7b
chandanch <chandan.cec107@gmail.com>
2024-04-22 09:32:31 +0530

    update background paper color

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -4,7 +4,7 @@ import React, { FC, ReactElement } from 'react';
 const Dashboard: FC = (): ReactElement => {
     return (
         <Grid container minHeight="100vh" m={0} p={0}>
-            <Grid item md={8} sm={12} xs={12}>
+            <Grid item md={8} sm={12} xs={12} px={4}>
                 <h2>Todos List Area</h2>
             </Grid>
             <Grid
