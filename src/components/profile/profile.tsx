74da02f33565b3780ef41c6dd2c192ec41484cc8
chandanch <chandan.cec107@gmail.com>
2024-04-27 12:50:15 +0530

    add avatar in profile component

--- src/components/profile/profile.tsx
+++ src/components/profile/profile.tsx
@@ -1,7 +1,26 @@
 import React, { FC, ReactElement } from 'react';
+import { Avatar, Box } from '@mui/material';
 
 const Profile: FC = (): ReactElement => {
-    return <p>Profile</p>;
+    return (
+        <Box
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+        >
+            <Avatar
+                sx={{
+                    width: '96px',
+                    height: '96px',
+                    backgroundColor: 'primary.main',
+                    marginBottom: '16px',
+                }}
+            >
+                CB
+            </Avatar>
+        </Box>
+    );
 };
 
 export default Profile;
