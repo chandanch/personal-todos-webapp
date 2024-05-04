6acdbca86de5b25c8a16572589f55eade7b21400
chandanch <chandan.cec107@gmail.com>
2024-05-04 15:56:36 +0530

    add createTask in sidebar comp :tada:

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,10 +1,12 @@
 import React, { FC, ReactElement } from 'react';
 import Profile from '../profile/profile';
+import CreateTask from '../createTask/createTask';
 
 const SideBar: FC = (): ReactElement => {
     return (
         <div>
             <Profile name="Mario" />
+            <CreateTask />
         </div>
     );
 };
