c3bcb888012881ef40aa2b807c223247a45cedd2
chandanch <chandan.cec107@gmail.com>
2024-05-08 14:37:44 +0530

    rename file createtaskform comp file

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,6 +1,6 @@
 import React, { FC, ReactElement } from 'react';
 import Profile from '../profile/profile';
-import CreateTaskForm from '../createTask/createTask';
+import CreateTaskForm from '../createTask/createTaskForm';
 
 const SideBar: FC = (): ReactElement => {
     return (
