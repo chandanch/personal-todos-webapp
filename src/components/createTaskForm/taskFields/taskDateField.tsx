67302015d4ab43b621a6241f0b755e664e5cc7ab
chandanch <chandan.cec107@gmail.com>
2024-05-26 08:52:50 +0530

    create date field component

--- src/components/createTaskForm/taskFields/taskDateField.tsx
+++ src/components/createTaskForm/taskFields/taskDateField.tsx
@@ -1,7 +1,23 @@
-import React, { FC, ReactElement } from 'react';
+import React, { FC, ReactElement, useState } from 'react';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { DesktopDatePicker } from '@mui/x-date-pickers';
 
 const TaskDateField: FC = (): ReactElement => {
-    return <div>Due Date</div>;
+    const [dueDate, setDueDate] = useState<Date | null>(null);
+
+    return (
+        <>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <DesktopDatePicker
+                    label="Due Date"
+                    format="dd/MM/yyyy"
+                    value={dueDate}
+                    onChange={(newValue) => setDueDate(newValue)}
+                />
+            </LocalizationProvider>
+        </>
+    );
 };
 
 export default TaskDateField;
