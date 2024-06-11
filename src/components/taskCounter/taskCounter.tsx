92d7ca47161df3a8b6d9ae136aa958887b5ec771
chandanch <chandan.cec107@gmail.com>
2024-06-11 07:41:12 +0530

    implement task counter UI

--- src/components/taskCounter/taskCounter.tsx
+++ src/components/taskCounter/taskCounter.tsx
@@ -1,7 +1,22 @@
+import { Avatar, Box, Typography } from '@mui/material';
 import React, { FC, ReactElement } from 'react';
 
 const TaskCounter: FC = (): ReactElement => {
-    return <div>Counter</div>;
+    return (
+        <>
+            <Box
+                display="flex"
+                flexDirection="column"
+                justifyContent="center"
+                alignItems="center"
+            >
+                <Avatar>
+                    <Typography>10</Typography>
+                </Avatar>
+                <Typography>Subtitle</Typography>
+            </Box>
+        </>
+    );
 };
 
 export default TaskCounter;
