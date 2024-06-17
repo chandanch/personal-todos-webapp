f6c9522932f26196f92f397b9aa61763942ef523
chandanch <chandan.cec107@gmail.com>
2024-06-17 11:45:41 +0530

    make task header dynamic with props

--- src/components/task/taskHeader.tsx
+++ src/components/task/taskHeader.tsx
@@ -1,14 +1,18 @@
 import { Box, Chip, Typography } from '@mui/material';
 import React, { FC, ReactElement } from 'react';
+import { ITaskHeader } from './interfaces/ITaskHeader';
+import { format } from 'date-fns';
 
-const TaskHeader: FC = (): ReactElement => {
+const TaskHeader: FC<ITaskHeader> = (props): ReactElement => {
+    const { title, taskDate } = props;
+
     return (
         <Box display="flex" width="100%" justifyContent="space-between" mb={4}>
             <Box>
-                <Typography>Get Spare Tyres</Typography>
+                <Typography>{title}</Typography>
             </Box>
             <Box>
-                <Chip variant="filled" label="June 12 2024" />
+                <Chip variant="filled" label={format(taskDate, 'PPP')} />
             </Box>
         </Box>
     );
