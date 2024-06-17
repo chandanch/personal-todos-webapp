d43bef0d255908ffc407585751e066c62ac9c575
chandanch <chandan.cec107@gmail.com>
2024-06-17 09:39:05 +0530

    add box container to task header

--- src/components/task/taskHeader.tsx
+++ src/components/task/taskHeader.tsx
@@ -1,7 +1,13 @@
+import { Box } from '@mui/material';
 import React, { FC, ReactElement } from 'react';
 
 const TaskHeader: FC = (): ReactElement => {
-    return <>Task header</>;
+    return (
+        <Box display="flex" width="100%" justifyContent="space-between" mb={4}>
+            <Box>Task Title</Box>
+            <Box>Date</Box>
+        </Box>
+    );
 };
 
 export default TaskHeader;
