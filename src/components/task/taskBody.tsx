6791be4326a6677342f5a228b398f137712b7eba
chandanch <chandan.cec107@gmail.com>
2024-06-21 08:44:38 +0530

    create task body comp

---
+++ src/components/task/taskBody.tsx
@@ -0,0 +1,12 @@
+import React, { FC, ReactElement } from 'react';
+import { Box, Typography } from '@mui/material';
+
+const TaskBody: FC = (): ReactElement => {
+    return (
+        <Box>
+            <Typography>Task Body</Typography>
+        </Box>
+    );
+};
+
+export default TaskBody;
