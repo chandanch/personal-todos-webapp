38d955a80411b41a5bf991f87385f20a60192895
chandanch <chandan.cec107@gmail.com>
2024-06-22 10:33:07 +0530

    add styles to task footer

--- src/components/task/taskFooter.tsx
+++ src/components/task/taskFooter.tsx
@@ -1,8 +1,28 @@
-import { Box } from '@mui/material';
+import { Box, Button, FormControlLabel, Switch } from '@mui/material';
 import React, { FC, ReactElement } from 'react';
 
 const TaskFooter: FC = (): ReactElement => {
-    return <Box>Footer</Box>;
+    return (
+        <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            mt={4}
+        >
+            <FormControlLabel
+                control={<Switch color="warning" />}
+                label="In Progress"
+            />
+            <Button
+                variant="contained"
+                color="success"
+                size="small"
+                sx={{ color: '#ffffff' }}
+            >
+                Mark as Done
+            </Button>
+        </Box>
+    );
 };
 
 export default TaskFooter;
