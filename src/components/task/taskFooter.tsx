0a4b0eea4ea2eb42969e02131c68645a1ae3c721
chandanch <chandan.cec107@gmail.com>
2024-06-22 10:17:25 +0530

    init task footer :clamp:

---
+++ src/components/task/taskFooter.tsx
@@ -0,0 +1,8 @@
+import { Box } from '@mui/material';
+import React, { FC, ReactElement } from 'react';
+
+const TaskFooter: FC = (): ReactElement => {
+    return <Box>Footer</Box>;
+};
+
+export default TaskFooter;
