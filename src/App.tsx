3d969139ebd85e9665104c4c851f118c7d1759e9
chandanch <chandan.cec107@gmail.com>
2024-07-10 06:53:07 +0530

    integrate react query and devtools :star:

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import { ThemeProvider, CssBaseline } from '@mui/material';
 import React, { FC, ReactElement } from 'react';
 import { customTheme } from './theme/customTheme';
 import Dashboard from './pages/dashboard/dashboard';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient: QueryClient = new QueryClient();
 
 const App: FC = (): ReactElement => {
     return (
-        <ThemeProvider theme={customTheme}>
-            <CssBaseline />
-            <Dashboard />
-        </ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+            <ThemeProvider theme={customTheme}>
+                <CssBaseline />
+                <Dashboard />
+            </ThemeProvider>
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
     );
 };
 
