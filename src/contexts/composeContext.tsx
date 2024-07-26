32f7897aa78cc00f76595139ddb4ea2d99a31831
chandanch <chandan.cec107@gmail.com>
2024-07-26 13:05:37 +0530

    implement compose context

--- src/contexts/composeContext.tsx
+++ src/contexts/composeContext.tsx
@@ -7,4 +7,12 @@ interface IComposeContext {
 
 export function ComposeContext(props: IComposeContext) {
     const { components = [], children } = props;
+
+    return (
+        <>
+            {components.reduceRight((acc, Comp: any) => {
+                return <Comp>{acc}</Comp>;
+            }, children)}
+        </>
+    );
 }
