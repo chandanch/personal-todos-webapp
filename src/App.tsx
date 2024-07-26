8396039d2d4f3f31430c078f754bfb5083ec738b
chandanch <chandan.cec107@gmail.com>
2024-07-26 13:33:49 +0530

    bind compose context to root app component

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,20 @@ import { customTheme } from './theme/customTheme';
 import Dashboard from './pages/dashboard/dashboard';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ComposeContext } from './contexts/composeContext';
+import { rootContexts } from './contexts/rootContext';
 
 const queryClient: QueryClient = new QueryClient();
 
 const App: FC = (): ReactElement => {
     return (
         <QueryClientProvider client={queryClient}>
-            <ThemeProvider theme={customTheme}>
-                <CssBaseline />
-                <Dashboard />
-            </ThemeProvider>
+            <ComposeContext components={rootContexts}>
+                <ThemeProvider theme={customTheme}>
+                    <CssBaseline />
+                    <Dashboard />
+                </ThemeProvider>
+            </ComposeContext>
             <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
     );
